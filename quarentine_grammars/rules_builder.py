d602d6b49ec394da7276965c9808fbfa15033c41
CatalinaRubio <catalina.rubio@telefonica.com>
2020-10-31 19:38:49 -0300

    [refactor] Refactor del armado de las reglas léxicas para mayor legibilidad

--- quarentine_grammars/rules_builder.py
+++ quarentine_grammars/rules_builder.py
@@ -1,48 +1,73 @@
+class LexicalRule():
+    
+    def build_lexical_rule(self):
+        feats = str(getattr(self,f"_{self.category}_feats")()).replace("'","").replace("\\\\","\\")
+        lex_rule = f"{self.category}{feats} -> '{self.shape}'"
+        return lex_rule
+
+    def _V_feats(self):
+        feats = [
+            f"SUBCAT={self.subcategoria}",
+            f"MODE={self.modo}",
+            f"TENSE={self.tiempo}",
+            f"PER={self.persona}",
+            f"NUM={self.numero}",
+            f"SEM=<\e.({self.lemma}(e) & {self.tiempo}(e))>"
+        ]
+        return feats
+
+    def _N_feats(self):
+        feats = [
+            f"NUM={self.numero}",
+            f"GEN={self.genero}",
+            f"SEM=<\\x.({self.lemma}(x))>"
+        ]
+        return feats
+
+    def _pronoun_feats(self):
+        feats = [
+            f"NUM={self.numero}",
+            f"GEN={self.genero}",
+            f"SEM=<\\x.(de-{self.funcion}-persona(x))>"
+        ]
+        return feats
+
+    def _A_feats(self):
+        if subcategoria == "pron":
+            feats = self._pronoun_feats(self)
+        else:
+            feats = [
+                f"NUM={self.numero}",
+                f"GEN={self.genero}",
+                f"FUNCTION={self.funcion}",
+                f"SEM=<\\x.({self.lemma}(x))>"
+            ]
+        return feats
+
 class NoMapping(Exception):
     pass
 
-
-class MakeRule:
+class MakeRule(LexicalRule):
     
     def __init__(self,word_dict,tags_mapping):
         
-        self.tag = word_dict.get("tag")
+        tag = word_dict.get("tag")
         self.lemma = word_dict.get("lema")
         self.shape = word_dict.get("forma")
-        self.category = self.tag[0]
-        self.mapping = tags_mapping.get(self.category)
+        self.category = tag[0]
+        mapping = tags_mapping.get(self.category)
 
-        if self.mapping:
-
-            for k in self.mapping.keys():
-                setattr(self,k,self._get_mapped_tag(k))
-
-            self.rule = self._make_rule()
-            
+        if mapping:
+            for k in mapping.keys():
+                setattr(self,k,self._get_mapped_tag(mapping.get(k),tag))
+            self.rule = self._make_rule()            
         else:
             raise NoMapping(f"No mapping for {self.category}")
                 
-    def _get_mapped_tag(self,rasgo):
-        rasgo_mapping = self.mapping.get(rasgo)
+    def _get_mapped_tag(self,rasgo_mapping,tag):
         position_tag = rasgo_mapping.get("posicion")
-        return rasgo_mapping.get(self.tag[position_tag],self.tag[position_tag])
+        return rasgo_mapping.get(tag[position_tag],tag[position_tag])
                 
     def _make_rule(self):
-        if self.category == "V":
-            rule = f"{self.category}[SUBCAT={self.subcategoria},MODE={self.modo},"\
-                    f"TENSE={self.tiempo},PER={self.persona},"\
-                    f"NUM={self.numero},SEM=<\e.({self.lemma}(e) "\
-                    f"& {self.tiempo}(e))>] -> '{self.shape}'"
-        elif self.category == "N":
-            rule = f"{self.category}[NUM={self.numero},GEN={self.genero},"\
-                    f"SEM=<\\x.({self.lemma}(x))>] -> '{self.shape}'"
-        elif self.category == "A":
-            if self.subcategoria != "pron":
-                rule = f"{self.category}[NUM={self.numero},GEN={self.genero},"\
-                    f"SUBCAT={self.subcategoria}, FUNCTION={self.funcion},"\
-                    f"SEM=<\\x.({self.lemma}(x))>] -> '{self.shape}'"
-            else:
-                rule = f"{self.category}[NUM={self.numero},GEN={self.genero},"\
-                    f"SUBCAT={self.subcategoria},"\
-                    f"SEM=<\\x.(de-{self.funcion}-persona(x))>] -> '{self.shape}'"
+        rule = self.build_lexical_rule()
         return rule
